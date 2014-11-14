1320fbaf8bdc537a805163621d6a7463de07f792
404pnf <404pnf@exmaple.org>
2014-11-14 15:00:03 +0800

    生成代码文档

--- yishu-yilian.js
+++ yishu-yilian.js
@@ -9,6 +9,8 @@ YSYL.debug = true;
 (function () {
   'use strict';
 
+  // ----
+  // ## 一些帮助函数
   var note;
   note = function (msg) {
     if (YSYL.debug) {
@@ -16,14 +18,16 @@ YSYL.debug = true;
     }
   };
 
-
+  // ----
+  // ## 处理选书页面的函数
   YSYL.xuanshu = function () {
 
     var hideTaskAssign, showTaskAssign, addBooks, tianjiaBooks;
 
+    // ### 隐藏和显示相应区块
+    // 开始的时候，用户没有选择任何书。
+    // 隐藏con_box1区块，显示 no_book区块。
     hideTaskAssign = function () {
-      // 开始的时候，用户没有选择任何书。
-      // 隐藏con_box1区块，显示 no_book区块。
       $('.task_assign1 .con_box1').addClass('displayn');
       $('.task_assign1 .no_book').removeClass('displayn');
     };
@@ -34,22 +38,15 @@ YSYL.debug = true;
       $('.task_assign1 .no_book').addClass('displayn');
     };
 
+    // ----
+    // ### 获取选择的图书，并串联它们的html内容
     // 1. 获取用户选择的书籍
     // 2. 获取这些书籍的div内容
     // 3. 删除原始选择的书籍
     // 4. 将书籍div内容插入到已选图书框中
-    // 每本图书div的结构
-    // 通过checkbox获取对象后需要往上走两步才能获得全书的div
-    // <li>
-    //     <div class="bookg_box">
-    //         <div class="bookg_img">
-    //         <input type="checkbox" class="bookg_input" />
-    //         <img src="none" />
-    //         </div>
-    //         <div class="bookg_tit">Sam's Pot</div><!--做...处理-->
-    //         <div class="bookg_tit_c"><span>5级</span><span>500词</span></div>
-    //     </div>
-    // </li>
+    //
+    // 注意： 通过checkbox获取对象后需要往上走两步才能获得全书的div。
+    //
     addBooks = function () {
       var checkedBoxes, htmlOfBooks;
 
@@ -58,24 +55,25 @@ YSYL.debug = true;
 
       htmlOfBooks = _.reduce(checkedBoxes, function (a, e) {
         var elementHTML, listElement;
+        // 通过checkbox获取对象后需要往上走三步才能获得全书的div的innerHTML，
+        // 而且获得的html不包括 li 标签，是它中间包含的内容。
         elementHTML = $(e).parent().parent().parent().html();
-        listElement = '<li>' + elementHTML + '<li>';
+        listElement = '<li>' + elementHTML + '</li>';
         return a + listElement;
       }, '');
       // note(htmlOfBooks);
 
+      // 是否移除选定的元素？
       // _.each(checkedBoxes, function (e) {
       //   $(e).parent().parent().remove();
       // });
 
-      // 插入html位置
-      // <div class="con_box1">
-      //   <div class="specification_sheet">
-      //         <ul class="bookg_ul oh" id="assign_books">
       $('#assign_books').html(htmlOfBooks);
 
     }; // addBooks ends
 
+    // ----
+    // ### 监听提交按钮，并调用相应函数
     tianjiaBooks = function () {
       $('#tj').click(function () {
         addBooks();
@@ -83,6 +81,8 @@ YSYL.debug = true;
       });
     };
 
+    // ----
+    // ### YSYL.xuanshu 这个函数的返回值
     return tianjiaBooks();
   }; // xuanshu ends
 
